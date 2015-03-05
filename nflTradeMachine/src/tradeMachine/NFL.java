04c58192a2f4fa9e2fc8e6523ff8a58f4527be2a
Derek <daotoo@iastate.edu>
2015-03-04 20:09:42 -0600

    Updated for iteration 1, java docs

--- nflTradeMachine/src/tradeMachine/NFL.java
+++ nflTradeMachine/src/tradeMachine/NFL.java
@@ -2,56 +2,140 @@ package tradeMachine;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ * NFL class that acts as the admin. Has control over all teams.
+ * 
+ */
 public class NFL {
-	
+
 	ArrayList<Team> teams = new ArrayList<Team>();
-	ArrayList<Player> freeAgents = new ArrayList<Player>();
-	
-	public NFL(){
+	ArrayList<Player> players = new ArrayList<Player>();
+
+	/**
+	 * Constructor to create NFL instance
+	 * 
+	 */
+	public NFL() {
 		this.teams = teams;
-		this.freeAgents = freeAgents;
-		
+		this.players = players;
+
 	}
 
+	/**
+	 * @return the teams as an array list.
+	 */
 	public ArrayList<Team> getTeams() {
 		return teams;
 	}
 
+	/**
+	 * @param teams
+	 *            - teams to set.
+	 */
 	public void setTeams(ArrayList<Team> teams) {
 		this.teams = teams;
 	}
-	public ArrayList<Player> getFreeAgents(){
-		return freeAgents;
+
+	/**
+	 * @return the players in the NFL as an array list.
+	 */
+	public ArrayList<Player> getPlayers() {
+		return players;
 	}
-	
-	public void setFreeAgents(ArrayList<Player> freeAgents){
-		this.freeAgents = freeAgents;
+
+	/**
+	 * @param players
+	 *            - players to set.
+	 */
+	public void setPlayers(ArrayList<Player> players) {
+		this.players = players;
 	}
-	
-	
-	public boolean addTeam(String id, String name){
-		return false;
-		
-	}
-	
-	public boolean addPlayers(ArrayList<String> ids, ArrayList<String> names){
-		return false;
-		
-	}
-	
-	public boolean setCap(String id, Integer cap){
-		return false;
-		
-	}
-	
-	public ArrayList<Player> getPlayers(){
-		ArrayList<Player> playersToReturn = new ArrayList<Player>();
-		
-		for(int i = 0; i < teams.size(); i++){
-			playersToReturn.addAll(teams.get(i).getPlayers());
+
+	/**
+	 * Adds a team to the NFLs list of teams.
+	 * 
+	 * @param team
+	 *            - the team to add to the list
+	 * @return true if the team was successfully added, false otherwise.
+	 */
+	public boolean addTeam(Team team) {
+		if (teams.contains(team)) {
+			return false;
+		} else {
+			teams.add(team);
+			return true;
 		}
-		
-		return playersToReturn;
+
+	}
+
+	/**
+	 * Adds a list of players to the NFL players list.
+	 * 
+	 * @param playersToAdd
+	 *            - The list of players to add.
+	 * @return true if the list of players was successfully added, false
+	 *         otherwise.
+	 */
+	public boolean addPlayers(ArrayList<Player> playersToAdd) {
+
+		ArrayList<String> playerIDs = getPlayerIDs(players);
+
+		for (int i = 0; i < playersToAdd.size(); i++) {
+			if (playerIDs.contains(players.get(i).getId())) {
+				return false;
+			}
+		}
+
+		players.addAll(playersToAdd);
+		return true;
+
+	}
+
+	/**
+	 * Sets the salaryCap for an NFL team
+	 * 
+	 * @param team
+	 *            - The team to set the salary cap
+	 * @param cap
+	 *            - the amount of money the cap will be set to
+	 * @return - true if the salary cap was successfully set, false otherwise.
+	 */
+	public boolean setCap(Team team, Integer cap) {
+		if (cap < 0 || cap > team.getSalaryCap()) {
+			return false;
+		}
+
+		team.setSalaryCap(cap);
+		return true;
+
+	}
+
+	/**
+	 * Returns the name of the team based on the given team.
+	 * 
+	 * @param team
+	 *            - The team to get the name of.
+	 * @return name of team as a string.
+	 */
+	public String getName(Team team) {
+		return team.getName();
+	}
+
+	/**
+	 * Private function for returning the IDs of all the players
+	 * 
+	 * @param players
+	 *            - the list of players to get IDs from
+	 * @return the IDs of all the players as an array list of strings.
+	 */
+	private ArrayList<String> getPlayerIDs(ArrayList<Player> players) {
+		ArrayList<String> playerIDs = new ArrayList<String>();
+		for (int i = 0; i < players.size(); i++) {
+			playerIDs.add(players.get(i).getId());
+		}
+
+		return playerIDs;
 	}
 
 }
--- nflTradeMachine/src/tradeMachine/Team.java
+++ nflTradeMachine/src/tradeMachine/Team.java
@@ -2,55 +2,159 @@ package tradeMachine;
 
 import java.util.ArrayList;
 
+/**
+ * Team class representing a team in the NFL.
+ * 
+ * 
+ */
 public class Team {
 	private ArrayList<Player> players = new ArrayList<Player>();
 	private int salaryCap;
 	private String name;
 	private String id;
-	
-	public Team(){
+	private int freeCapSpace;
+
+	/**
+	 * Constructor to create a Team instance.
+	 */
+	public Team() {
 		this.players = players;
 		this.salaryCap = salaryCap;
 		this.name = name;
-		this.id = id;		
+		this.id = id;
+		this.freeCapSpace = freeCapSpace;
 	}
 
+	/**
+	 * @return the free cap space a team has as an integer.
+	 */
+	public int getFreeCapSpace() {
+		return freeCapSpace;
+	}
+
+	/**
+	 * Sets the free cap space for a team
+	 * 
+	 * @param freeCapSpace
+	 *            - the free cap space to set for the team
+	 */
+	public void setFreeCapSpace(int freeCapSpace) {
+		this.freeCapSpace = freeCapSpace;
+	}
+
+	/**
+	 * Gets all the players from a team
+	 * 
+	 * @return a list of players associated with the team
+	 */
 	public ArrayList<Player> getPlayers() {
 		return players;
 	}
 
+	/**
+	 * Sets the players for a team
+	 * 
+	 * @param players
+	 *            - players to set
+	 */
 	public void setPlayers(ArrayList<Player> players) {
 		this.players = players;
 	}
 
+	/**
+	 * @return the salary cap for the team as an integer.
+	 */
 	public int getSalaryCap() {
 		return salaryCap;
 	}
 
+	/**
+	 * Sets the salary cap for the team
+	 * 
+	 * @param salaryCap
+	 *            - the salary cap for the team
+	 */
 	public void setSalaryCap(int salaryCap) {
 		this.salaryCap = salaryCap;
 	}
 
+	/**
+	 * @return the name of the team
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Sets the name of the Team
+	 * 
+	 * @param name
+	 *            - name of the team.
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * @return the id of the team as a string.
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * Sets the id of a team
+	 * 
+	 * @param id
+	 *            - the id to set for the team
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
-	
-	
-	public boolean addPlayer(String id, String name){
-		return false;
 
+	/**
+	 * Adds a player to the team.
+	 * 
+	 * @param player
+	 *            - the player to add to the team
+	 * @return true if the player was successfully added, false otherwise.
+	 */
+	public boolean addPlayer(Player player) {
+		if (players.contains(player)) {
+			return false;
+		} else {
+
+			players.add(player);
+			return true;
+
+		}
+
+	}
+
+	/**
+	 * Gets the number of players on the team
+	 * 
+	 * @param teamID
+	 *            - the id of the team as a string
+	 * @return the number of players on the team, null if team does not exist.
+	 */
+	public int getNumberOfPlayers(String teamID) {
+		if (teamID == id) {
+			return players.size();
+		}
+
+		else
+			return (Integer) null;
+	}
+
+	public Player getPlayer(String playerID) {
+		for (int i = 0; i < players.size(); i++) {
+			if (players.get(i).getId() == playerID) {
+				return players.get(i);
+			}
+		}
+
+		return (Player) null;
 	}
 
 }
