010e6df923563d1e87b995d2f86688f3fcfcf6dc
Cavanaugh, Sean <Cavanaugh.Sean@principal.com>
2015-04-28 14:04:58 -0500

    nfl to static

--- nflTradeMachine/src/tradeMachine/NFLTradeMachine.java
+++ nflTradeMachine/src/tradeMachine/NFLTradeMachine.java
@@ -1,42 +1,42 @@
 package tradeMachine;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
-
 import java.util.Scanner;
 
 public class NFLTradeMachine {
-	static Scanner scanner;
-	
+    static private Scanner scanner;
+    static private NFLManagerController nflManagerController;
+
     public static void main(String[] args) {
-        NFLManagerController nflManagerController = new NFLManagerController();
+        nflManagerController = new NFLManagerController();
         String input = "";
         scanner = new Scanner(System.in);
         System.out.println("Hello and welcome to the NFL Trade Machine!");
         System.out.println("--------------------------------------------\n");
-        while(!input.equals("Exit") && !input.equals("exit")) {
+        while (!input.equals("Exit") && !input.equals("exit")) {
             System.out.println("Please enter one of the following commands: trade, getTeam, getPlayer, getPlayersFromTeam, exit");
             input = scanner.next();
-            if(input.equals("getTeam")) {
-            	
+            if (input.equals("getTeam")) {
+
             }
-            
-            
+
+
         }
     }
-    
+
     public void trade() {
-    	
+
     }
-    
-    public void getTeam(String) {
-    	
+
+    public void getTeam() {
+
     }
-    
+
     public void getPlayer() {
-    	
+
     }
-    
+
     public void getPlayersFromTeam() {
-    	
+        System.out.println("Please enter a team name to get the players from:");
+        String teamName = scanner.next();
     }
 }
